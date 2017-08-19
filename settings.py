aca7356bce8f62f605f237180d39c63795a748b8
Krittam <krittam@gmail.com>
2017-08-19 11:48:59 +0530

    add example settings file

---
+++ settings.py
@@ -0,0 +1,10 @@
+mail_server = 'smtp.iitd.ernet.in'
+mail_port = 25
+
+entry_no = '2014TT10abc'
+tnp_pwd = '*********'
+kerberos_id = 'tt1140abc'
+kerberos_pwd = '*********'
+
+from_mail = 'tnpnotifications@gmail.com'
+to_mail = 'username@anymail.com'
