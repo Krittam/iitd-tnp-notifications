a87e99d7cb96e216c8a999ef3325e856f46d2b8f
Krittam <krittam@gmail.com>
2017-10-28 13:30:22 +0530

    add ppt and status schange scripts

--- status_sync.py
+++ status_sync.py
@@ -5,26 +5,26 @@ import os
 import time
 from mail_utils import MailUtils
 from company_parser import CompanyParser
-from settings import from_mail, to_mail, sleep_time, company_url, tnp_pwd, entry_no, login_url
-hash_file_name = 'hash_company'
+from settings import from_mail, to_mail,status_url, sleep_time, tnp_pwd, entry_no, login_url
+hash_file_name = 'hash_status'
 
 while True:
     mailer = None
     session = None
     try:
-
         mailer = MailUtils()        
-        mail_params = {'subject': 'You might have new Companies on TnP portal !','from':from_mail,'to':to_mail}
+        mail_params = {'subject': 'TnP Application status changed !','from':from_mail,'to':to_mail}
         
         session = requests.Session()
         payload = {'username': entry_no, 'password': tnp_pwd, 'login': 'Submit'}
         response = session.post(login_url, data=payload, verify=False)
 
-        page = session.get(company_url, verify=False)
+        page = session.get(status_url, verify=False)
         
-        parser = CompanyParser()        
+        parser = CompanyParser()
         parser.feed(page.text)
-        companies = parser.get_companies()        
+        companies = parser.get_companies()
+        print(companies)
         hash_str = hashlib.sha256(str(companies).encode('utf-8')).hexdigest()
         if not os.path.exists(os.path.join(os.path.dirname(os.path.realpath(__file__)),hash_file_name)):
             open(hash_file_name, 'w+').close()
@@ -50,8 +50,8 @@ while True:
         log +=str(sys.exc_info()[0])
         print("Exception occured{}".format(log))
         if mailer :
-            mail_params['subject'] = 'TnP company sync script crashed !'
-            mail_params['body'] = 'Unfortunately your TnP Company sync script crashed !\nHere is the exception traceback\n{}'.format(log)
+            mail_params['subject'] = 'TnP status sync script crashed !'
+            mail_params['body'] = 'Unfortunately your TnP status sync script crashed !\nHere is the exception traceback\n{}'.format(log)
             mailer.send_mail(from_mail,to_mail,mail_params)
         if session:
             session.close()
